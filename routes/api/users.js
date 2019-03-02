f44574bb6bd805ba7db3bb4e1c8d3e3458130b59
Alex Cho <alexsjcho@gmail.com>
2019-03-02 09:25:57 -0800

    created User Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// @route GET api/posts/test
+// @route GET api/users/test
 // @description Tests post route
 // @access Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
